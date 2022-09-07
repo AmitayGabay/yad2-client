408b220b810e29b4620a7729c4ea07db8fc5e589
Amitay <amitaygabay1@gmail.com>
2022-09-07 20:25:28 +0300

    first of all

---
+++ src/components/home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <h1>welcome to home page please start coding</h1>
+  );
+};
+
+export default Home;
---
+++ src/shared/layout/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from "./header.module.css"
+console.log(style)
+
+const Header = () => {
+    return (
+        <h4 className={style.anotherStile}>do header and navBar</h4>
+    )
+}
+
+export default Header
