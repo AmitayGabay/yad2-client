747d81701fe423accee845f8b31df12017410eb8
Amitay <amitaygabay1@gmail.com>
2022-11-03 10:14:19 +0200

    first commit

--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,32 @@
-import React, { useContext } from "react";
-import contextStore from "../shared/context/contextStore";
-import { Container, Grid } from '@mui/material';
+import React, { useContext, useEffect, useRef } from "react";
+import { Container, Grid, Box } from '@mui/material';
 import Product from "./product/Product";
+import useLazyLoading from "react-simple-lazy-loading";
+import { GET_PRODUCTS_ROUTE } from "../shared/constant/url";
 
 const Home = () => {
-  const { products } = useContext(contextStore)
-  // get products
+  const loadingRef = useRef(null);
+  const options = {
+    refTriger: loadingRef,
+    rootMargin: "100px",
+    threshold: 1.0,
+    url: GET_PRODUCTS_ROUTE
+  }
+
+  const { data, loading, page } = useLazyLoading(options);
+
+  useEffect(() => {
+    console.log(data, loading, page);
+  }, [data])
+
   return (
     <Container maxWidth='xl'>
       <Grid container spacing={3}>
-        {products?.map((item, i) =>
+        {data?.map((item, i) =>
           <Product product={item} key={i} />
         )}
       </Grid>
+      <Box ref={loadingRef} sx={{height: "200px", background: "red"}}></Box>
     </Container>
   );
 };
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,19 +1,24 @@
 import React from 'react'
-import { Grid, Paper, Box, Typography, Rating } from '@mui/material';
-import style from "./product.module.css";
-
+import { Grid, Paper, Box, Typography, Rating, Button } from '@mui/material';
+import "./product.css";
+import { BsCartPlus } from "react-icons/bs"
 const Product = ({ product }) => {
+
     return (
         <Grid item lg={3} md={4} sm={6} xs={12}>
-            <Paper sx={{ paddingBottom: "4px" }} elevation={4}>
-                <Box className={style.productImage} sx={{ backgroundImage: "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSG_TB1R7WQQj3k8YOcqQ9zKnPYKfQyPMnj2w&usqp=CAU)" }}></Box>
+            <Paper sx={{ paddingBottom: "18px" }} elevation={4}>
+                <Box className="product-productImage" sx={{ backgroundImage: "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSG_TB1R7WQQj3k8YOcqQ9zKnPYKfQyPMnj2w&usqp=CAU)" }}></Box>
                 <Box sx={{ direction: "rtl" }} overflow={'hidden'} paddingX={1}>
                     <Typography variant='h5' component="h2">מעיל חורף</Typography>
                     <Typography sx={{ fontSize: "17px", lineHeight: "18px" }} whiteSpace={"pre-wrap"} variant='subtitle2' component="h2">תיאור המוצר: מוצר חדש, נמכר עקב חוסר בשימוש</Typography>
                     <Typography variant="h6" component="h2">מחיר: ₪50</Typography>
                 </Box>
                 <Box paddingX={1} display={"flex"} justifyContent={"end"}>
-                    <Rating onChange={(e, value) => console.log(value)} name="half-rating" defaultValue={2.3} precision={0.5} />
+                    <Rating onChange={(e, value) => console.log(value)} name="half-rating" defaultValue={2.3} precision={0.5} readOnly />
+                </Box>
+                <Box marginTop={1} display={"flex"} alignItems={"center"} justifyContent="space-evenly">
+                    <Button variant="contained" className="product-cartBtn"><BsCartPlus style={{ color: "black" }} size={31} /></Button>
+                    <Button className="product-buyNowBtn" variant="contained">קנה עכשיו</Button>
                 </Box>
             </Paper>
         </Grid>
